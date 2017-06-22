4396444fcf61b305e62778fd485068799f27d6d0
Jazmín <jazmin@Jazmins-MacBook-Pro.local>
2017-06-22 12:03:01 -0500

    javascript completed

---
+++ keys.js
@@ -0,0 +1,15 @@
+/* ASSIGNMENT #8: LIRI BOT */
+
+// console.log("this is loaded");
+
+exports.twitterKeys = {
+	consumer_key: "Fhdjc1PlsclvGPjy5iEJT9nFi",
+	consumer_secret: "22J2fgzVADDtriWNUn5h9XoI4sX89a3lr2J2J51NQQ7fRYFoQ4",
+	access_token_key: "877190825146556417-IUcRuoBsX1tM7aWuX2UVhUxYitIgetV",
+	access_token_secret: "fLWhUolFszkKdcZKK8TUTrrYFRlRHCL1pUK4wJlBWxzL0"
+}
+
+exports.spotifyKeys = {
+	client_id: "83219936878b4442b7adb6e429ed6d91",
+	client_secret: "b07ef1e83f104541af2cfe8747f1488b"
+}
